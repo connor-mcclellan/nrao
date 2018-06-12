7393f42c2969bc738402cb4a53bcca3aa89fd84e
Brian McClellan <bmcclell@nmpost030.aoc.nrao.edu>
2018-06-12 11:54:15 -0600

    fixed file formatting for outputs, began iterative process on dendrograms

--- dendrocat.py
+++ dendrocat.py
@@ -11,16 +11,16 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-def contour(infile, min_value=0.0001, min_delta=0.0002, min_npix=10, plot=True):
+def contour(infile, min_value=0.0001, min_delta=0.0002, min_npix=10, plot=True, verbose=True):
     
-    outfile = 'dend_val{}_delt{}_pix{}'.format(min_value, min_delta, min_npix)
+    outfile = 'dend_val{:.2E}_delt{:.2E}_pix{}'.format(min_value, min_delta, min_npix).replace("E-0", "E-")
     contfile = fits.open(infile)                        # load in fits image
     da = contfile[0].data.squeeze()                     # get rid of extra axes
 
     mywcs = wcs.WCS(contfile[0].header).celestial       # set up world coordinate system, ditch extra dimensions
     beam = radio_beam.Beam.from_fits_header(contfile[0].header)
 
-    d = Dendrogram.compute(da, min_value=min_value, min_delta=min_delta, min_npix=min_npix, wcs=mywcs, verbose=True)
+    d = Dendrogram.compute(da, min_value=min_value, min_delta=min_delta, min_npix=min_npix, wcs=mywcs, verbose=verbose)
     pixel_scale = np.abs(mywcs.pixel_scale_matrix.diagonal().prod())**0.5 * u.deg
     pixel_scale_as = pixel_scale.to(u.arcsec).value
 
@@ -35,9 +35,10 @@ def contour(infile, min_value=0.0001, min_delta=0.0002, min_npix=10, plot=True):
             }
 
     cat = pp_catalog(d, metadata)                       # set up position-position catalog
-    cat.pprint(show_unit=True, max_lines=10)            # display to check values
+    if verbose:
+        cat.pprint(show_unit=True, max_lines=10)        # display to check values
     
-    with open('./reg/reg_'+outfile+'.reg', 'w') as fh:               # write catalog information to region file
+    with open('./reg/reg_'+outfile+'.reg', 'w') as fh:  # write catalog information to region file
 	    fh.write("fk5\n")
 	    for row in cat:
 	        fh.write("ellipse({x_cen}, {y_cen}, {major_sigma}, {minor_sigma}, {position_angle}) # text={{{_idx}}}\n".format(**dict(zip(row.colnames, row))))
@@ -48,8 +49,11 @@ def contour(infile, min_value=0.0001, min_delta=0.0002, min_npix=10, plot=True):
         plt.imshow(da, cmap='gray_r', interpolation='none', origin='lower',
                   vmax=0.01, vmin=-0.001)
         pltr = d.plotter()
-        print("Plotting contours to PDF...")
-        pb = ProgressBar(len(d.leaves))
+        
+        if verbose:
+            print("Plotting contours to PDF...")
+            pb = ProgressBar(len(d.leaves))
+            
         for struct in d.leaves:                         # iterate over each of the leaf structures
             pltr.plot_contour(ax, structure=struct, colors=['r'],
                               linewidths=[0.9], zorder=5)
@@ -58,7 +62,9 @@ def contour(infile, min_value=0.0001, min_delta=0.0002, min_npix=10, plot=True):
                     struct = struct.parent
                 pltr.plot_contour(ax, structure=struct, colors=[(0,1,0,1)],
                                   linewidths=[0.5])
-            pb.update()
+            if verbose:
+                pb.update()
+                
         cntr = plt.gca().collections
 
         plt.setp([x for x in cntr if x.get_color()[0,0] == 1], linewidth=0.25)
--- exec_script.py
+++ exec_script.py
@@ -2,4 +2,14 @@ from dendrocat import *
 
 infilename = '/lustre/aoc/students/bmcclell/w51/W51e2_cont_briggsSC_tclean.image.fits.gz'
 
-contour(infilename, min_value=0.001, min_delta=0.002)
+min_values = np.linspace(0.0001, 0.001, 5)
+min_deltas = min_values*2.
+min_npixs = [10, 15, 20]
+
+print('Calculating dendrograms across specified parameters...')
+pb = ProgressBar(len(min_values)*len(min_npixs))
+
+for i in range(len(min_values)):
+    for j in range(len(min_npixs)):
+        contour(infilename, min_value=min_values[i], min_delta=min_values[i], min_npix=min_npixs[j], verbose=False)
+        pb.update()

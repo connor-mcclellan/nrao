682ceded7936bda1424b22dbab12e6b4298ed4b1
Brian McClellan <bmcclell@nmpost030.aoc.nrao.edu>
2018-06-25 09:47:39 -0600

    added 'detected_bandX' column to reject.py output'

--- flux.py
+++ flux.py
@@ -1,1 +1,15 @@
+# import the catalog file, use filename to acquire the correct image files
+
+# set up columns for new aperture photometry measurements
+
+# for loop iterating over each row in the catalog
+
+    # load in this star's ellipse properties from the catalog
+    
+    # create all apertures
+
+
+
+
+
 ellipse_flux = MaskedColumn(length=len(catalog), name='ellipse_flux', mask=True)
